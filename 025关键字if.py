0bf52731b77888a92f963d67fe54d2c39e695a02
7117 <qinglangsalaheiyou@163.com>
2023-04-01 23:35:55 +0800

    关键字if

--- 025关键字if.py
+++ 025关键字if.py
@@ -2,8 +2,15 @@ gender = False
 if gender:
     print('你是一个不错的人')
 
-
 if gender:
-     print('你是不错的')
+    print('你是不错的')
 else:
-     print('我不错')
+    print('我不错')
+
+if gender == True:
+    print(gender)
+elif gender == False:
+    print(gender)
+else:
+    print(gender)
+
