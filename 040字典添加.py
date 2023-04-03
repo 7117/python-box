3457ffc1c7d8a44b40a217398766376dd90d69e2
7117 <qinglangsalaheiyou@163.com>
2023-04-03 23:22:17 +0800

    字典

---
+++ 040字典添加.py
@@ -0,0 +1,8 @@
+per = {'name': 'aaa'}
+print(per)
+
+per['age'] = 11
+print(per)
+
+per['name'] = 'bbb'
+print(per)
---
+++ 041字典删除.py
@@ -0,0 +1,10 @@
+per = {'name': 'aaa', 'age': 12}
+
+del per['name']
+print(per)
+
+per.clear()
+print(per)
+
+del per
+
